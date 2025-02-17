1e37366dc758d7eab544f3d272ec32d4e036ecb7
cadumd <cadu.md2021@gmail.com>
2025-02-17 10:07:42 -0300

    jhjh

--- script.js
+++ script.js
@@ -1,55 +1,75 @@
-const musicaDoCombate = new Audio('./audio/bgm.mp3')
-musicaDoCombate.play()
-
 const player1 = document.querySelector('#player1')
 const player2 = document.querySelector('#player2')
 const vidaPlayer1 = document.querySelector('#vida-player1')
 const vidaPlayer2 = document.querySelector('#vida-player2')
 const numeroTirado = document.querySelector('p')
 const button = document.querySelector('button')
-
+let contagemMusica = 0
 
 function sortearQualPlayerTomaODano(min, max) {
+    if (contagemMusica === 0) {
+        const musicaDoCombate = new Audio('./audio/bgm.mp3')
+        musicaDoCombate.play()
+        contagemMusica++
+    }
+
     let sorteio = Math.floor(Math.random() * (max - min + 1)) + min;
     if (sorteio === 1) {
-        player1.classList.add('player1-animation')
-        const musicaDoCombate = new Audio('./audio/blue.mp3')
-        musicaDoCombate.play()
+        loop(1, 20)
         sortearDano(1, 20, player1)
-        setTimeout(() => {
-            player1.classList.remove('player1-animation')
-        }, 500);
     } else {
-        player2.classList.add('player2-animation')
-        const musicaDoCombate = new Audio('./audio/red.mp3')
-        musicaDoCombate.play()
+        loop(1, 20)
         sortearDano(1, 20, player2)
-        setTimeout(() => {
-            player2.classList.remove('player2-animation')
-        }, 500);
     }
 }
 
-function sortearDano(min, max, playerAnonimo) {
-    let sorteio = Math.floor(Math.random() * (max - min + 1)) + min;
-    numeroTirado.textContent = sorteio
-    if (playerAnonimo === player1) {
-        let quantidadeDeVida = vidaPlayer1.clientWidth
-        let vidaAtual = quantidadeDeVida - sorteio
-        vidaPlayer1.style.width = vidaAtual + "px"
-        if (vidaAtual <= 0) {
-            window.alert("player1 perdeu o combat")
+function loop(min, max) {
+    let contagemLoop = 0
+    setInterval(() => {
+        if (contagemLoop < 20) {
+            let sorteio = Math.floor(Math.random() * (max - min + 1)) + min;
+            numeroTirado.textContent = sorteio
+            contagemLoop++
         }
-    }
+    }, 100);
+}
 
-    if (playerAnonimo === player2) {
-        let quantidadeDeVida = vidaPlayer2.clientWidth
-        let vidaAtual = quantidadeDeVida - sorteio
-        vidaPlayer2.style.width = vidaAtual + "px"
-        if (vidaAtual <= 0) {
-            window.alert("player2 perdeu o combat")
+function sortearDano(min, max, playerAnonimo) {
+    setTimeout(() => {
+        let sorteio = Math.floor(Math.random() * (max - min + 1)) + min;
+        numeroTirado.textContent = sorteio
+        if (playerAnonimo === player1) {
+            player1.classList.add('player1-animation')
+            const musicaDoCombate = new Audio('./audio/blue.mp3')
+            musicaDoCombate.play()
+            setTimeout(() => {
+                player1.classList.remove('player1-animation')
+            }, 500);
+
+            let quantidadeDeVida = vidaPlayer1.clientWidth
+            let vidaAtual = quantidadeDeVida - sorteio
+            vidaPlayer1.style.width = vidaAtual + "px"
+            if (vidaAtual <= 0) {
+                window.alert("player1 perdeu o combat")
+            }
         }
-    }
+
+        if (playerAnonimo === player2) {
+            player2.classList.add('player2-animation')
+            const musicaDoCombate = new Audio('./audio/red.mp3')
+            musicaDoCombate.play()
+            setTimeout(() => {
+                player2.classList.remove('player2-animation')
+            }, 500);
+
+            let quantidadeDeVida = vidaPlayer2.clientWidth
+            let vidaAtual = quantidadeDeVida - sorteio
+            vidaPlayer2.style.width = vidaAtual + "px"
+            if (vidaAtual <= 0) {
+                window.alert("player2 perdeu o combat")
+            }
+        }
+    }, 2000);
 }
 
 button.addEventListener('click', () => {
